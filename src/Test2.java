620e07b88b7f4452c2212fa7925c3f119068db24
Filizbagdat <149052999+Filizbagdat@users.noreply.github.com>
2024-03-26 12:30:27 +0100

    Update Test2.java

--- src/Test2.java
+++ src/Test2.java
@@ -1,5 +1,6 @@
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("Test2  add to project2");
+        System.out.println("Test2 first update add to project2");
     }
 }
