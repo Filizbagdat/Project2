4168c2bf72730d26f8072a5c697359fa6b0499bc
Filiz Bagdatli <melihahalim@hotmail.de>
2024-03-26 12:25:45 +0100

    add test1  update to project2

--- src/Test1.java
+++ src/Test1.java
@@ -2,5 +2,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println(" project2 test1 ");
         System.out.println("project2 tes1 update");
+        System.out.println("project2 test1  second update");
     }
 }
